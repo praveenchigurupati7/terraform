08fd6ce2e45ad9aa70bb63d4d382f7eae7287988
praveen chigurupati <praveen.chigurupati@actifio.com>
2023-10-30 23:03:16 -0500

    adding firewall rule

--- kubernetes/gke/network/main.tf
+++ kubernetes/gke/network/main.tf
@@ -11,3 +11,15 @@ resource "google_compute_subnetwork" "subnet" {
   network       = google_compute_network.vpc.name
   ip_cidr_range = "10.10.0.0/24"
 }
+
+resource "google_compute_firewall" "allow_https" {
+  name    = "allow-https-traffic"
+  network = google_compute_network.vpc.name
+
+  source_ranges = ["0.0.0.0/0"]
+  priority = "900"
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443", "9090", "30000-32767"]
+  }
+}
